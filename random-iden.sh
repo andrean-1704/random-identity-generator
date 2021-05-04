5bdf4ecd8701ab3aaccb05ad52b9a28cf911d341
andrean-1704 <andreankulo@outlook.com>
2021-05-04 15:46:43 +0700

    remove dynamic directory

--- random-iden.sh
+++ random-iden.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir dynamic
 shuf -n 2 data/namas | tr '\n' ' ' > dynamic/junk
 sed -e 's/ *$//g' dynamic/junk > dynamic/junk2
 sed 's/\ .*//' dynamic/junk2 > dynamic/junk3
@@ -7,3 +8,4 @@ echo ---; printf 'name : '; cat dynamic/junk2
 printf '\nid   : ';cat dynamic/junk3|tr [:upper:] [:lower:];cat dynamic/ranam
 printf 'dob  : ';python3 data/random-date.py
 printf 'pass : ';shuf -n 1 data/pass|tr [:upper:] [:lower:]|tr -d \\n;shuf -i 1000-9999 -n 1;echo ---
+rm -r dynamic
