ef4de97148b463182d379e6904e390887ae94423
andrean-1704 <82682027+andrean-1704@users.noreply.github.com>
2021-05-20 15:04:05 +0700

    back to python 3

--- random-iden.sh
+++ random-iden.sh
@@ -6,6 +6,6 @@ sed 's/\ .*//' dynamic/junk2 > dynamic/junk3
 shuf -i 1000-9999 -n 1 > dynamic/ranam
 echo ---; printf 'name : '; cat dynamic/junk2
 printf '\nid   : ';cat dynamic/junk3|tr [:upper:] [:lower:];cat dynamic/ranam
-printf 'dob  : ';python data/random-date.py
+printf 'dob  : ';python3 data/random-date.py
 printf 'pass : ';shuf -n 1 data/pass|tr [:upper:] [:lower:]|tr -d \\n;shuf -i 1000-9999 -n 1;echo ---
 rm -r dynamic
